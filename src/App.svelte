eec3c711388674503013f604a65bfbce307c4fc4
Ignacio Mallaviabarrena <71989922+nachomallavia@users.noreply.github.com>
2025-03-28 22:20:09 -0300

    Add App component

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import RealEstateProduct from './lib/RealEstateProduct.svelte';
+</script>
+
+<main>
+  <RealEstateProduct />
+</main>
+
+<style>
+:global(body) {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  background-color: #f8fafc;
+}
+
+main {
+  min-height: 100vh;
+  padding: 2rem 0;
+}
+</style>
